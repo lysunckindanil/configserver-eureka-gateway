e35824df870f26fa0e4e3d30d6eff94b90a0f59a
Danila <danil.lysunckin@gmail.com>
2025-01-08 13:04:52 +0500

    setting Sampler = always sample

--- producer-service/src/main/java/org/example/producerservice/ProducerServiceApplication.java
+++ producer-service/src/main/java/org/example/producerservice/ProducerServiceApplication.java
@@ -1,8 +1,10 @@
 package org.example.producerservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class ProducerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProducerServiceApplication.class, args);
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
     }
 
 }
